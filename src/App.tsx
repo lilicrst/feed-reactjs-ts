f67576919d16c146072c5d36834e0724be979cb8
lilicrst <lisandra.crisilva37@gmail.com>
2022-08-25 17:10:40 -0300

    fix error in post

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,21 @@ import styles from './App.module.css';
 import './global.css';
 import { Sidebar } from "./components/Sidebar";
 
-const posts = [
+interface Posts {
+  id: number;
+  author: {
+    avatarUrl: string;
+    name: string;
+    role: string
+  },
+  content: {
+    type: 'paragraph' | 'link';
+    content: string
+  }[],
+  publishedAt: Date
+}
+
+const posts:Posts[] = [
   {
     id: 1,
     author: {
